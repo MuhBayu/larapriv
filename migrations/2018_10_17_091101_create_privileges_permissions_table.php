ba2b37b8ea732128ac76c891c55cea94fd007406
MuhBayu <bnugraha00@hotmail.com>
2018-11-09 17:37:43 +0700

    change: classname migration

--- migrations/2018_10_17_091101_create_privileges_permissions_table.php
+++ migrations/2018_10_17_091101_create_privileges_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPrivilegesRolesTable extends Migration
+class CreatePrivilegesPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
